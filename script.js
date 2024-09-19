fd1d4bfab5a06372408d19231a3a78dc6b67e98c
nishatmahzabin <159991238+nishatmahzabin@users.noreply.github.com>
2024-09-19 23:57:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const assessBtn = document.getElementById('assessBtn');
-  const assessmentResult = document.getElementById('Result');
+function MultiplicationTable() {
+  const number = document.getElementById('numberInput').value;
+  const resultDiv = document.getElementById('result');
 
-  assessBtn.addEventListener('click', VotingEligibility);
+  const num = Number(number);
 
-  function VotingEligibility() {
-      let userYears = prompt("Please enter your age in years:");
-      
-      if (isNaN(userYears) || parseInt(userYears) < 0) {
-          displayResult("Invalid age. Please enter a positive number.");
-      } else {
-          let ageValue = parseInt(userYears);
-          if (ageValue >= 18) {
-              displayResult("You are eligible to  vote.");
-          } else {
-              displayResult("You aren't yet eligible to vote.");
-          }
-      }
+  let table = `\n Multiplication table for ${num}:\n\n`;
+  for (let i = 1; i <= 10; i++) {
+      table += `${num} x ${i} = ${num * i}\n`;
   }
 
-  function displayResult(message) {
-      assessmentResult.textContent = message;
-  }
-});
+  
+  resultDiv.textContent = table;
+}document.getElementById('generateBtn').addEventListener('click', MultiplicationTable);
